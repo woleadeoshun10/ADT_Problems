c8310bb3f5c66c4bab23aef3e3833112d195c3ee
Adewole Adeoshun <146618781+woleadeoshun10@users.noreply.github.com>
2025-07-15 00:33:58 -0400

    Update Binary_Search_Index_Finder.cpp

--- Binary_Search_Index_Finder.cpp
+++ Binary_Search_Index_Finder.cpp
@@ -8,36 +8,3 @@ Your implementation should be efficient and should follow the binary search algo
 */
 
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-// Function to perform binary search
-int binarySearch(vector<int>& nums, int target){
-  int first = 0;
-  int last = nums.size() - 1;
-  
-  while(first <= last){
-    int mid = (first + last) / 2;
-    if (nums[mid] == target){
-      return mid;
-    }
-    else if (nums[mid] < target){
-      first = mid + 1;
-    }
-    else{
-      last = mid - 1;
-    }
-  }
-  return -1;
-}
-
-int main() {
-    vector<int> nums = {1, 3, 5, 7, 9, 11};
-    int target = 7;
-
-    int result = binarySearch(nums, target);
-    cout << "Target index: " << result << endl;
-
-    return 0;
-}
